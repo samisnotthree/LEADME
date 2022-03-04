7beaf1e8e0f1b541ce0fbfe0c83d5af8181f42f1
unknown <samisnotthree@gmail.com>
2022-03-04 21:45:48 +0900

    findByEmail

--- core/src/main/java/com/leadme/core/repository/MemberRepository.java
+++ core/src/main/java/com/leadme/core/repository/MemberRepository.java
@@ -2,7 +2,6 @@ package com.leadme.core.repository;
 
 import com.leadme.core.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
