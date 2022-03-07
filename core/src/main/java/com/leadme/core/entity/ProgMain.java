f4cf23646984c4c88fc143142c92aa13a2d128f4
unknown <samisnotthree@gmail.com>
2022-03-07 19:41:46 +0900

    add ProgMain

---
+++ core/src/main/java/com/leadme/core/entity/ProgMain.java
@@ -0,0 +1,22 @@
+package com.leadme.core.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class ProgMain {
+    @Id
+    @GeneratedValue
+    private Long progId;
+
+    private String name;
+    private String desc;
+    private Integer maxMember;
+    private String duration;
+    private Long price;
+    private String prepMat;
+    private String meetLocation;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "guide_id")
+    private Guide guide;
+}
