18153490979b8ec5f5da76d3cccd381a05d42544
unknown <samisnotthree@gmail.com>
2022-03-08 20:35:43 +0900

    Hashtag test

--- core/src/test/java/com/leadme/core/service/HashtagServiceTest.java
+++ core/src/test/java/com/leadme/core/service/HashtagServiceTest.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.NoSuchElementException;
+
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -34,5 +36,26 @@ class HashtagServiceTest {
         // then
         assertThat(savedHashtag.getName()).isEqualTo("서울");
         assertThat(hashtag1).isEqualTo(savedHashtag);
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("해시태그 삭제(delete)")
+    void deleteTest() {
+        // given
+        Hashtag hashtag = new Hashtag();
+        hashtag.setName("서울");
+
+        // when
+        Hashtag savedHashtag = hashtagRepository.save(hashtag);
+        hashtagRepository.delete(savedHashtag);
+        System.out.println("@@@@");
+        //Hashtag hashtag1 = hashtagRepository.findById(1L).get();
+
+        // then
+        // 좋은 테스트가 아님
+        org.junit.jupiter.api.Assertions.assertThrows(NoSuchElementException.class, () -> {
+            hashtagRepository.findById(1L).get();
+        });
     }
 }
