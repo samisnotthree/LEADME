bff0d6ab7ec29fceb29ed631476ceba841a08fa6
samisnotthree <samisnotthree@gmail.com>
2022-03-14 01:14:46 +0900

    update OrderService

--- core/src/main/java/com/leadme/core/repository/OrderRepository.java
+++ core/src/main/java/com/leadme/core/repository/OrderRepository.java
@@ -1,8 +1,11 @@
 package com.leadme.core.repository;
 
 import com.leadme.core.entity.Orders;
+import com.leadme.core.entity.ProgDaily;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderRepository extends JpaRepository<Orders, Long> {
+import java.util.List;
 
+public interface OrderRepository extends JpaRepository<Orders, Long> {
+    List<Orders> findByProgDaily(ProgDaily progDaily);
 }
--- core/src/main/java/com/leadme/core/service/OrderService.java
+++ core/src/main/java/com/leadme/core/service/OrderService.java
@@ -1,10 +1,14 @@
 package com.leadme.core.service;
 
 import com.leadme.core.entity.Orders;
+import com.leadme.core.entity.enums.OrderStatus;
 import com.leadme.core.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
@@ -16,6 +20,17 @@ public class OrderService {
     @Transactional
     public Long addOrder(Orders order) {
 
-        return 1L;
+
+        order.setOrderDate(LocalDateTime.now());
+        //TODO 결제 분리 시 결제 일시 로직 수정
+        order.setPayDate(LocalDateTime.now());
+        order.setStatus(OrderStatus.PAYED);
+        return orderRepository.save(order).getOrderId();
+    }
+
+    public boolean validateOrder(Orders order) {
+        List<Orders> foundProgDaily = orderRepository.findByProgDaily(order.getProgDaily());
+
+        return foundProgDaily.size() < order.getProgDaily().getProg().getMaxMember();
     }
 }
