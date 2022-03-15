7c730da73744153ad3704940410816f02dcba232
HyungEun Kwon <85722378+samisnotthree@users.noreply.github.com>
2022-03-15 14:31:47 +0900

    Update GuideHashtag.java

--- core/src/main/java/com/leadme/core/entity/GuideHashtag.java
+++ core/src/main/java/com/leadme/core/entity/GuideHashtag.java
@@ -1,12 +1,13 @@
 package com.leadme.core.entity;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
-@Getter @Setter
+@Getter
 public class GuideHashtag {
 
     @Id
@@ -21,4 +22,9 @@ public class GuideHashtag {
     @JoinColumn(name = "hashtag_id")
     private Hashtag hashtag;
 
+    @Builder
+    public GuideHashtag(Guide guide, Hashtag hashtag) {
+        this.guide = guide;
+        this.hashtag = hashtag;
+    }
 }
