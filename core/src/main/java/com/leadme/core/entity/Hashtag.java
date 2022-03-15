0d3183102bb1834854782d4f69e78a369bda1ec7
HyungEun Kwon <85722378+samisnotthree@users.noreply.github.com>
2022-03-15 14:36:13 +0900

    Update Hashtag.java

--- core/src/main/java/com/leadme/core/entity/Hashtag.java
+++ core/src/main/java/com/leadme/core/entity/Hashtag.java
@@ -1,5 +1,6 @@
 package com.leadme.core.entity;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -16,6 +17,11 @@ public class Hashtag {
     private Long HashtagId;
 
     private String name;
+    
+    @Builder
+    public Hashtag(String name) {
+        this.name = name;
+    }
 
     public void changeHashtag(String name) {
         this.name = name;
