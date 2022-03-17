871ce4b6c33d120a7e21d273dce289c88748236d
HyungEun Kwon <85722378+samisnotthree@users.noreply.github.com>
2022-03-17 13:17:38 +0900

    Update ProgDailyDto.java

--- core/src/main/java/com/leadme/core/dto/ProgDailyDto.java
+++ core/src/main/java/com/leadme/core/dto/ProgDailyDto.java
@@ -4,7 +4,26 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
 public class ProgDailyDto {
-
+    private Long progDailyId;
+    private LocalDateTime progDate;
+    private Prog prog;
+  
+    @Builder
+    public ProgDailyDto(LocalDateTime progDate, Prog prog) {
+        this.progDate = progDate;
+        this.prog = prog;
+    }
+  
+    public ProgDaily toEntity() {
+        return ProgDaily.builder()
+          .progDate(progDate)
+          .prog(prog)
+          .build();
+    }
+  
+    public ProgDailyDto(ProgDaily progDaily) {
+        this.progDate = progDaily.getProgDate();
+        this.prog = progDaily.getProg();
+    }
 }
