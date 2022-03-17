dd638017d17e8bfdf15035f3920a6fae4c4d5482
samisnotthree <samisnotthree@gmail.com>
2022-03-17 23:44:28 +0900

    check

--- core/src/main/java/com/leadme/core/controller/MemberController.java
+++ core/src/main/java/com/leadme/core/controller/MemberController.java
@@ -6,14 +6,12 @@ import com.leadme.core.service.MemberService;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
@@ -39,6 +37,6 @@ public class MemberController {
     @Data
     @AllArgsConstructor
     static class Result<T> {
-        private T data;
+        private T members;
     }
 }
