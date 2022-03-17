35347b0634341cf8d3387cca829187c32098a481
HyungEun Kwon <85722378+samisnotthree@users.noreply.github.com>
2022-03-17 11:00:52 +0900

    Update HashtagDto.java

--- core/src/main/java/com/leadme/core/dto/HashtagDto.java
+++ core/src/main/java/com/leadme/core/dto/HashtagDto.java
@@ -1,4 +1,23 @@
 package com.leadme.core.dto;
 
+@Getter
 public class HashtagDto {
+    private Long hashtagId;
+    private String name;
+    
+    @Builder
+    public HashtagDto(String name) {
+        this.name = name;
+    }
+    
+    public Hashtag toEntity() {
+        return Hashtag.builder()
+          .name(name)
+          .build();
+    }
+    
+    public HashtagDto(Hashtag hashtag) {
+        this.hashtagId = hashtag.getHashtagId();
+        this.name = hashtag.getName();
+    }
 }
