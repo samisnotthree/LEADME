16b9a5840bebcdadcfffbc0e29ebba6013b25bb5
HyungEun Kwon <85722378+samisnotthree@users.noreply.github.com>
2022-03-17 10:53:15 +0900

    Update GuideDto.java

--- core/src/main/java/com/leadme/core/dto/GuideDto.java
+++ core/src/main/java/com/leadme/core/dto/GuideDto.java
@@ -1,4 +1,34 @@
 package com.leadme.core.dto;
 
 public class GuideDto {
+    private Long guideId;
+    private String desc;
+    private LocalDateTime inDate;
+    private LocalDateTime outDate;
+    private Member member;
+  
+    @Builder
+    public GuideDto(String desc, LocalDateTime inDate, LocalDateTime outDate, Member member) {
+        this.desc = desc;
+        this.inDate = inDate;
+        this.outDate = outDate;
+        this.member = member;
+    }
+    
+    public Guide toEntity() {
+        return Guide.builder()
+          .desc(desc)
+          .inDate(inDate)
+          .outDate(outDate)
+          .member(member)
+          .build();
+    }
+    
+    public GuideDto(Guide guide) {
+        this.guideId = guide.getGuideId();
+        this.desc = guide.getDesc();
+        this.inDate = guide.getInDate();
+        this.outDate = guide.getOutDate();
+        this.member = guide.getMember();
+    }
 }
