8e8580c693ea0e7947f6e401d392db3d28ecea92
samisnotthree <samisnotthree@gmail.com>
2022-03-21 20:42:06 +0900

    update GuideController

--- core/src/main/java/com/leadme/core/controller/GuideController.java
+++ core/src/main/java/com/leadme/core/controller/GuideController.java
@@ -1,4 +1,22 @@
 package com.leadme.core.controller;
 
+import com.leadme.core.repository.GuideRepository;
+import com.leadme.core.service.GuideService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class GuideController {
+    private final GuideService guideService;
+    private final GuideRepository guideRepository;
+
+    @Transactional
+    @PostMapping("/joinGuide")
+    public Long joinGuide(@RequestBody Long memberId, @RequestBody String desc) {
+        return guideService.joinGuide(memberId, desc).getGuideId();
+    }
 }
