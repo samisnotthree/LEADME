4f930735d5402179a6b2f615e4955dea959df684
HyungEun Kwon <85722378+samisnotthree@users.noreply.github.com>
2022-03-22 16:21:52 +0900

    Update HashtagController.java

--- core/src/main/java/com/leadme/core/controller/HashtagController.java
+++ core/src/main/java/com/leadme/core/controller/HashtagController.java
@@ -1,4 +1,34 @@
 package com.leadme.core.controller;
 
+@RestController
+@RequiredArgsConstructor
 public class HashtagController {
+    private final HashtagService hashtagService;
+    private final HashtagRepository hashtagRepository;
+  
+    @Transactional
+    @PostMapping("/addHashtag")
+    public Long addHashtag(@RequestBody HashtagDto hashtagDto) {
+        return hashtagService.addHashtag(hashtagDto.toEntity());
+    }
+    
+    @GetMapping("/findHashtag")
+    public Result findAll() {
+        return new Result(hashtagRepository.findAll()
+                .stream()
+                .map(HashtagDto::new)
+                .collect(Collectors.toList()));
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T hashtags;
+    }
+  
+    @Transactional
+    @PostMapping("deleteHashtag")
+    public void deleteHashtag(@RequestBody HashtagDto hashtagDto) {
+        hashtagService.deleteHashtag(hashtagDto.toEntity());
+    }
 }
