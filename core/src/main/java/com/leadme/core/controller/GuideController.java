880a7ceed637e15976e8ebd6c9dbbd49c03fb524
samisnotthree <samisnotthree@gmail.com>
2022-03-23 18:34:01 +0900

    update GuideController

--- core/src/main/java/com/leadme/core/controller/GuideController.java
+++ core/src/main/java/com/leadme/core/controller/GuideController.java
@@ -1,12 +1,19 @@
 package com.leadme.core.controller;
 
+import com.leadme.core.dto.GuideDto;
+import com.leadme.core.entity.Guide;
 import com.leadme.core.repository.GuideRepository;
 import com.leadme.core.service.GuideService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,5 +25,19 @@ public class GuideController {
     @PostMapping("/joinGuide")
     public Long joinGuide(@RequestBody Long memberId, @RequestBody String desc) {
         return guideService.joinGuide(memberId, desc).getGuideId();
+    }
+
+    @GetMapping("findGuide")
+    public Result findGuide(Guide guide) {
+        return new Result(guideRepository.findById()
+                .stream()
+                .map(GuideDto::new)
+                .collect(Collectors.toList()));
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T members;
     }
 }
