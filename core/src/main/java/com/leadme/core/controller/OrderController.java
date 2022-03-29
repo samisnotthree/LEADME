2643a3cbf473f56f225ea79928488a1e25c0f650
HyungEun Kwon <85722378+samisnotthree@users.noreply.github.com>
2022-03-29 13:25:38 +0900

    Update OrderController.java

--- core/src/main/java/com/leadme/core/controller/OrderController.java
+++ core/src/main/java/com/leadme/core/controller/OrderController.java
@@ -1,4 +1,14 @@
 package com.leadme.core.controller;
 
+@RestController
+@RequiredArgsConstructor
 public class OrderController {
+    private final OrderService orderService;
+    private final OrderRepository orderRepository;
+  
+    @Transactional
+    @PostMapping("/order")
+    public Long addOrder(@RequestBody OrderDto orderDto) {
+        return orderService.addOrder(orderDto.toEntity());
+    }
 }
