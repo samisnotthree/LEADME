214d744351ce5c63ec037a981b2a4d0802cc8924
unknown <samisnotthree@gmail.com>
2022-04-13 19:49:29 +0900

    update wrong mapping

--- core/src/main/java/com/leadme/core/controller/ProgController.java
+++ core/src/main/java/com/leadme/core/controller/ProgController.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,7 +32,7 @@ public class ProgController {
     /**
      *  프로그램 조회
      */
-    @PostMapping("/prog")
+    @GetMapping("/prog")
     public Result findProgs(@RequestBody String desc) {
         return new Result(progRepository.findByDescContaining(desc)
                 .stream()
