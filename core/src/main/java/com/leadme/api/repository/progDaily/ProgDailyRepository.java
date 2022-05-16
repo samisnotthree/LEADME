b038edf6e4942b916f996a671d2fdaef508b6caa
HyungEun Kwon <85722378+samisnotthree@users.noreply.github.com>
2022-05-16 16:04:10 +0900

    Update ProgDailyRepository.java

--- core/src/main/java/com/leadme/api/repository/progDaily/ProgDailyRepository.java
+++ core/src/main/java/com/leadme/api/repository/progDaily/ProgDailyRepository.java
@@ -6,11 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ProgDailyRepository extends JpaRepository<ProgDaily, Long> {
-    List<ProgDaily> findByProgAndProgDate(Prog prog, LocalDateTime progDate);
+    List<ProgDaily> findByProgAndProgDate(Prog prog, String progDate);
     
     @Query(value = "select pd from ProgDaily pd join fetch pd.prog p where p.progId = :id and formatdatetime(pd.progDate, 'yyyyMMdd') = :progDate")
     public List<ProgDaily> findSchedules(@Param(value = "id") Long progId, @Param(value = "progDate") String progDate);
